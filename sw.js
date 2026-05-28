f3d3d41b42389b0331e2b427d3db0f399b54e375
accounting4 <accounting@elitetrade.club>
2026-05-28 16:09:16 -0400

    Bump SW cache to v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'etc-dashboard-v7';
+const CACHE_NAME = 'etc-dashboard-v8';
 const ASSETS = ['/etc-pnl-report/', '/etc-pnl-report/index.html'];
 
 self.addEventListener('install', e => {
