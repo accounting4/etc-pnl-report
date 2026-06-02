9733c51568fc1d6ce8775af1d113915515174940
accounting4 <accounting@elitetrade.club>
2026-06-02 13:53:49 -0400

    Change 67: Dynamic MTD month label + Revenue & Forecast uses MTD sources

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'etc-dashboard-v15';
+const CACHE_NAME = 'etc-dashboard-v16';
 const ASSETS = ['/etc-pnl-report/', '/etc-pnl-report/index.html'];
 
 self.addEventListener('install', e => {
