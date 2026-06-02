05ae72db967137ba64247e05093724c5c786bd50
accounting4 <accounting@elitetrade.club>
2026-06-02 14:22:27 -0400

    Fix Change 67: render order + revTotal ref + init _mtdGrandByMonth + exclude RPP

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'etc-dashboard-v17';
+const CACHE_NAME = 'etc-dashboard-v18';
 const ASSETS = ['/etc-pnl-report/', '/etc-pnl-report/index.html'];
 
 self.addEventListener('install', e => {
