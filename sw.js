bfc4cd28210bb3d1253d38be04123333d761b8de
accounting4 <accounting@elitetrade.club>
2026-06-29 11:33:40 -0400

    Bump SW cache to v31 to force browser refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'etc-dashboard-v30';
+const CACHE_NAME = 'etc-dashboard-v31';
 const ASSETS = ['/etc-pnl-report/', '/etc-pnl-report/index.html'];
 
 self.addEventListener('install', e => {
